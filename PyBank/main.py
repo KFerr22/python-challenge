2b7ddb83d14fe10d18788b92cf436a426bacdc3f
KFerr22 <karinferrada@y7mail.com>
2022-04-05 23:28:28 +0800

    Final upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,58 @@
-print("Hello")
+#import from source file and read csv file
+import os
+import csv
+
+csvpath = os.path.join("Resources","budget_data.csv")
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    header = next(csvreader)
+    
+#create list
+    month_count = []
+    profit = []
+    change_profit = []
+                      
+#add values to list 
+    for row in csvreader:
+        month_count.append(row[0])
+        profit.append(int(row[1]))
+    for i in range(len(profit)-1):
+        change_profit.append(profit[i+1]-profit[i])
+                      
+#max and min profits and dates
+increase = max(change_profit)
+decrease = min(change_profit)
+
+month_increase = change_profit.index(max(change_profit))+1
+month_decrease = change_profit.index(min(change_profit))+1
+
+#calculate average change, greatest increase and decrease dates
+average_change = round(sum(change_profit)/len(change_profit),2)
+greatest_increase_date = month_count[month_increase]
+greatest_decrease_date = month_count[month_decrease]
+
+
+print("Financial Analysis")
+print("------------------------")
+print(f"Total Months:{len(month_count)}")
+print(f"Total: ${sum(profit)}")
+print(f"Average Change:{(average_change)}")
+print(f"Greatest Increase in Profits: {greatest_increase_date} (${(str(increase))})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${(str(decrease))})")     
+
+# save the output file path as a txt file to Analysis folder
+output = os.path.join("Analysis", 'output.txt')
+with open(output,"w") as new:
+    new.write("Financial Analysis")
+    new.write("\n")
+    new.write("----------------------------")
+    new.write("\n")
+    new.write(f"Total Months:{len(month_count)}")
+    new.write("\n")
+    new.write(f"Total: ${sum(profit)}")
+    new.write("\n")
+    new.write(f"Average Change: {(average_change)}")
+    new.write("\n")
+    new.write(f"Greatest Increase in Profits: {greatest_increase_date} (${(str(increase))})")
+    new.write("\n")
+    new.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${(str(decrease))})")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,80 @@
+#import from source file and read csv file
+import os
+import csv
 
+csvpath = os.path.join("Resources","election_data.csv")
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    header = next(csvreader)
+
+# create list
+    vote_list = []
+    candidates = []
+    percent_candidate = []
+    count_candidate = []
+
+# create count
+    total_vote = 0
+
+    for row in csvreader:
+        vote_list.append(row[2])
+        total_vote = len(vote_list)
+
+# find candidates 
+for name in vote_list:
+   if name not in candidates:
+        candidates.append(name)
+        i = name
+
+# total votes counter
+count = 0
+# loop to set candidate from vote_list
+candidate = vote_list[0]
+
+last_count = 0
+# print election results total votes
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_vote}")
+print("-------------------------")
+
+for candidate in candidates:
+    for vote in vote_list:
+        if candidate == vote:
+            count += 1
+    percent = count / len(vote_list)
+    percent_candidate.append(percent)
+    count_candidate.append(count)
+    
+    if last_count < count:
+        winner = candidate    
+    print(f"{candidate}: {percent:.3%} ({count})")
+    
+    # reset vote count to zero
+    last_count = count
+    count = 0
+
+# print winner
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
+
+# save the output file path as a txt file to Analysis folder
+output = os.path.join("Analysis", 'output.txt')
+with open(output,"w") as new:
+    new.write("Election Results")
+    new.write("\n")
+    new.write("-------------------------")
+    new.write("\n")
+    new.write(f"Total Votes: {total_vote}")
+    new.write("\n")
+    new.write("-------------------------")
+    new.write("\n")
+    for candidate in candidates:
+        index = candidates.index(candidate)
+        new.write(f"{candidate}: {percent_candidate[index]:.3%} ({count_candidate[index]})\n")
+    new.write("-------------------------")
+    new.write("\n")
+    new.write(f"Winner: {winner}")
+    new.write("\n")
+    new.write("-------------------------")
